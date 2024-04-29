8b5321fd4c0dde9bc54bddea11b7dd96bfc85af2
Adil Soomro <adilgulsoomro@gmail.com>
2024-04-29 14:23:31 -0400

    Added Vue App

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 const product = 'Socks'
+
+const app = Vue.createApp({
+    data(){
+        return {
+            product: 'Socks',
+            description: 'These socks are comfortable and provide nice value'
+        }
+    }
+})
